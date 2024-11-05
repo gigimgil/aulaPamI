f4d515fde28d6fcf235bbb5dd6102d120fb16b16
gigimgil <giulia.gil@etec.sp.gov.br>
2024-11-05 15:21:12 -0300

    aula 5/11

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
 import Exemplo1 from './src/exemplos/ex_01';
+import Exemplo2 from './src/exemplos/ex_02';
 
 import Atividade1 from './src/atividades/atv_01';
+import Atividade2 from './src/atividades/atv_02';
+import Atividade3 from './src/atividades/atv_03';
+import Atividade4 from './src/atividades/atv_04';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade1/>
+      <Atividade2/>
       <StatusBar style="auto" />
     </View>
   );
